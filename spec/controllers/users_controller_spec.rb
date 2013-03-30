1b9787438de1035042bd31a5845cfad68491b9a0
Maks <maksim.karganov@gmail.com>
2013-03-30 14:50:28 -0700

    Full test coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe UsersController do
 
-let(:user) {User.create(:name => 'Maks')}
+let(:user) {User.create(:name => 'Maks', :email => "maks@test.com", :password => "123", :password_confirmation => "123")}
   context 'GET edit' do
     it 'should return @user' do
       get :edit, :id => user.id
@@ -28,8 +28,22 @@ let(:user) {User.create(:name => 'Maks')}
     end
 
     it 'should render edit template if params invalid' do
-      put :update, :id => user.id
-      response.should render_template('edit')
+      put :update, :id => user.id, :user => { :email => "", :name => "" }
+      response.should render_template(:edit)
+    end
+  end
+
+  context 'GET new' do
+    it 'should assign a new user' do
+   xhr :get, :new, :user => { :name => 'Bob', :email => 'bob@test.com', :password => '123', :password_confirmation => '123' }, :remote => :true
+   assigns[:user]
+    end
+  end
+
+  context 'POST create' do
+    it 'should create a new user from received params' do
+
+    expect {xhr :post, :create, :user => { :name => 'Bob', :email => 'bob@test.com', :password => '123', :password_confirmation => '123' }}.to change { User.count }.by(1)
     end
   end
 
